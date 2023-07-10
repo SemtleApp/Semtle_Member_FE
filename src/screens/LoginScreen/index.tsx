7d5536c3dcc8c2dcafd5f1196c65d48ad22cd1ec
onejuneee <onejuneee721@gmail.com>
2023-07-11 02:04:51 +0900

    [Feat] - #7 카카오로그인 화면분할

---
+++ src/screens/LoginScreen/index.tsx
@@ -0,0 +1,99 @@
+import React, {useCallback, useState} from 'react';
+import {
+  Button,
+  Pressable,
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import {
+  KakaoOAuthToken,
+  KakaoProfile,
+  KakaoProfileNoneAgreement,
+  login,
+  logout,
+  unlink,
+  getProfile,
+} from '@react-native-seoul/kakao-login';
+import styles from './styles';
+
+const LoginScreen = () => {
+  const [result, setResult] = useState<string>('');
+
+  const signInWithKakao = async (): Promise<void> => {
+    try {
+      const token: KakaoOAuthToken = await login();
+      console.log(token);
+
+      setResult(JSON.stringify(token));
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const signOutWithKakao = async (): Promise<void> => {
+    try {
+      const message = await logout();
+      console.log(message);
+
+      setResult(message);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const getKakaoProfile = async (): Promise<void> => {
+    try {
+      const profile: KakaoProfile | KakaoProfileNoneAgreement =
+        await getProfile();
+      console.log(profile);
+
+      setResult(JSON.stringify(profile));
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const unlinkKakao = async (): Promise<void> => {
+    try {
+      const message = await unlink();
+      console.log(message);
+
+      setResult(message);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>result : {result}</Text>
+      <Button
+        testID="btn-login"
+        onPress={() => signInWithKakao()}
+        title={'카카오 로그인'}
+      />
+      <View style={{marginTop: 12}} />
+      <Button
+        testID="btn-login"
+        onPress={() => getKakaoProfile()}
+        title={'프로필 조회'}
+      />
+      <View style={{marginTop: 12}} />
+      <Button
+        testID="btn-login"
+        onPress={() => unlinkKakao()}
+        title={'링크 해제'}
+      />
+      <View style={{marginTop: 12}} />
+      <Button onPress={() => signOutWithKakao()} title={'카카오 로그아웃'} />
+      <View style={{marginTop: 40}} />
+    </View>
+  );
+};
+
+export default LoginScreen;
---
+++ src/screens/LoginScreen/styles.tsx
@@ -0,0 +1,17 @@
+import {StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderColor: 'blue',
+    borderWidth: 5,
+  },
+  text: {
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+});
+
+export default styles;
