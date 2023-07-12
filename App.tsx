8b9ec33ffd2c15015c0f2fb5883d16df9646a2e5
jjjuyoa <jjjuyoa@gmail.com>
2023-07-12 20:28:53 +0900

    feat: header navigator에 적용

--- App.tsx
+++ App.tsx
@@ -6,16 +6,22 @@ import {createStackNavigator} from '@react-navigation/stack';
 
 import LoginScreen from './src/screens/LoginScreen/index'; // LoginScreen 컴포넌트의 경로에 맞게 수정해주세요
 
+import Header from './src/components/Header/Header';
+
+
 const Stack = createStackNavigator();
 
 const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator
+        screenOptions={{
+          header: () => <Header />,
+        }}>
         <Stack.Screen
           name="Login"
           component={LoginScreen}
-          options={{headerShown: false}}
+          // options={{headerShown: false}}
         />
         {/* 다른 화면들을 여기에 추가할 수 있습니다 */}
       </Stack.Navigator>
