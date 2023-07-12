93811875fa5b0efe11494296a6277e8c174cf1c8
jjjuyoa <jjjuyoa@gmail.com>
2023-07-12 12:29:59 +0900

    feat: 테스트용 스크린

---
+++ src/screens/NotificationsScreen.tsx
@@ -0,0 +1,11 @@
+import {Text, View} from 'react-native';
+
+const NotificationsScreen = () => {
+  return (
+    <View>
+      <Text>알림 페이지</Text>
+    </View>
+  );
+};
+
+export default NotificationsScreen;
---
+++ src/screens/ProfileScreen.tsx
@@ -0,0 +1,11 @@
+import {Text, View} from 'react-native';
+
+const ProfileScreen = () => {
+  return (
+    <View>
+      <Text>프로필 페이지</Text>
+    </View>
+  );
+};
+
+export default ProfileScreen;
