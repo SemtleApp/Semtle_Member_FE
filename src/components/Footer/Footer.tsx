0c1655b891274efe8130cfeda3e3daf4a729f64f
jjjuyoa <jjjuyoa@gmail.com>
2023-07-12 20:28:09 +0900

    feat: header, footer styles 분리

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,8 +1,10 @@
-import {View, StyleSheet, Text, Image} from 'react-native';
+import {View, Text, Image} from 'react-native';
 import React from 'react';
 
 const facebookImage = require('../../../assets/mobile_semtle_common_asset/footer/footer_facebook_icon.png');
 const instaImage = require('../../../assets/mobile_semtle_common_asset/footer/footer_insta_icon.png');
+
+import styles from './styles';
 const Footer = () => {
   return (
     <View style={styles.footer}>
@@ -18,32 +20,5 @@ const Footer = () => {
     </View>
   );
 };
-
-const styles = StyleSheet.create({
-  footer: {
-    position: 'absolute',
-    bottom: 0,
-    width: '100%',
-    height: 132,
-    backgroundColor: '#f5f5f5',
-  },
-  footerText: {
-    textAlign: 'center',
-    fontSize: 9,
-    lineHeight: 16,
-    fontWeight: '400',
-  },
-  footerIcon: {
-    flexDirection: 'row',
-    justifyContent: 'center',
-  },
-  footerImage: {
-    width: 12,
-    height: 12,
-  },
-  iconSeparator: {
-    width: 10, // 간격을 조정하기 위한 너비
-  },
-});
 
 export default Footer;
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,4 +1,4 @@
-import {Image, StyleSheet, Text, View, TouchableOpacity} from 'react-native';
+import {Image, Text, View, TouchableOpacity} from 'react-native';
 import React from 'react';
 
 const logoImage = require('../../../assets/mobile_semtle_common_asset/header/logo.png');
@@ -6,6 +6,7 @@ const notificationImage = require('../../../assets/mobile_semtle_common_asset/he
 const menuImage = require('../../../assets/mobile_semtle_common_asset/header/Vector.png');
 const profileImage = require('../../../assets/mobile_semtle_common_asset/header/profile_blue_img.png');
 
+import styles from './styles';
 const Header = () => {
   return (
     <View style={styles.header}>
@@ -27,44 +28,5 @@ const Header = () => {
     </View>
   );
 };
-
-const styles = StyleSheet.create({
-  header: {
-    width: '100%',
-    height: '17%',
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'flex-start',
-    borderBottomWidth: 1,
-    borderColor: '#C3C3C3',
-  },
-  headerText: {
-    fontWeight: '400',
-    fontSize: 17,
-    color: '#333',
-    letterSpacing: 1,
-  },
-  headerImage: {
-    height: 20,
-    width: 75,
-    marginLeft: 10,
-  },
-  centerContent: {
-    flex: 1,
-    alignItems: 'center',
-  },
-  rightContent: {
-    marginLeft: 'auto',
-    marginRight: 10,
-    flexDirection: 'row',
-  },
-  button: {
-    marginHorizontal: 5,
-    paddingVertical: 5,
-    paddingHorizontal: 3,
-    backgroundColor: '#00000000',
-    borderRadius: 5,
-  },
-});
 
 export default Header;
